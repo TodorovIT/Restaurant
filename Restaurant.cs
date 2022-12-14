fbf450dc46f3be3dba0593b58a60b8e4046b15fb
TodorovIT <120493772+TodorovIT@users.noreply.github.com>
2022-12-14 16:07:56 +0200

    Add files via upload

---
+++ Restaurant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Restaurant
+    {
+        private static string name;
+        private List<Staff> staff;
+        private List<Client> client;
+        private Menu menu;
+        private BigDecimal sum;
+        private int capacity;
+        private int tablesCount;
+        private List<Table> tables;
+        private Bar bar;
+        private Storage storage;
+    }
+}
